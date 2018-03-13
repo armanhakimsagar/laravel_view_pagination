6b4cbb3b74ad428b4adc3b8897b0e036200f78a2
Arman Hakim Sagar <armanhakimsagar@gmail.com>
2018-03-13 09:49:23 +0600

    Update laravel_view_search_pagination.php

--- laravel_view_search_pagination.php
+++ laravel_view_search_pagination.php
@@ -63,6 +63,20 @@ VIEW :
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
 
+
+.................................
+
+
+
+Order by pagiate :
+
+$research = DB::table('researches')->orderBy('id', 'desc')->paginate(10);
+
+
+
+
+
+
 --------------------------------
 
 Search :
